62b812de30c2fb215e8d646e8d56f1bb72c23d33
Pedro Santos <f.santosPedro225@gmail.com>
2025-03-25 15:51:29 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
 
-export default function App() {
+WebBrowser.maybeCompleteAuthSession();
+
+ function App() {
+    const [request, response, promptAsync] = Google.useAuthRequest({
+      clientId: '996272040634-o3cghkog6r8fpcpc3rqg67do8rsgpss1.apps.googleusercontent.com',
+    });
+  
+    // Verifica a resposta da autenticação
+    useEffect(() => {
+      if (response?.type === 'success') {
+        const { authentication } = response;
+        Alert.alert('Login realizado!', `Token: ${authentication.accessToken}`);
+        console.log(authentication);
+      }
+    }, [response]);
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      <LinearGradient
+        colors={["#000114", "#00032b", "#010970", "#040fd4"]} start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }} style={styles.container}
+      >
+        <StatusBar style='light'/>
+        <Text style={styles.titleApp}>SPOBRE-FY</Text>
+        
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        
+      </LinearGradient>
+    );
+  }
+  
+  export default App;
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "white",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    
+    titleApp: {
+      position: "absolute",
+      top: "35%",
+      color: "white",
+      fontSize: 32,
+      fontWeight: "bold",
+      color: "#05d8f0",
+    },
+    Image: {
+      width: 200,
+      height: 200,
+      position: "absolute",
+      top: "40%",
+    },
+  });
+  
+
+
+
